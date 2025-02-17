3889a7bc72526754b7f9abf977986f0ce66de0d5
anurag-nair16 <anuragnair1607.com>
2025-02-17 18:25:30 +0530

    updated gitignore

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -85,7 +85,8 @@ const LoginPage = () => {
   
       const loginData = { email, password };
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/login/`, {
+        // const response = await fetch(`${process.env.REACT_APP_API_URL}/login/`, {
+          const response = await fetch("http://127.0.0.1:8000/login/", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
