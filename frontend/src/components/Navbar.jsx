507671005a5d28f68e3f17545c29dc41e77b4726
anurag-nair16 <anuragnair1607.com>
2025-04-23 22:12:37 +0530

    added recipe generator

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -62,6 +62,7 @@ const Navbar = () => {
                     <NavItem icon={<FaHome />} text="Home" to="/" scrollY={scrollY} />
                     <NavItem icon={<FaUtensils />} text="Submit" to="/submit-recipe" scrollY={scrollY} />
                     <NavItem icon={<FaUser />} text="Meal Plan" to="/meal-planner" scrollY={scrollY} />
+                    <NavItem icon={<FaUser />} text="Recipe Generator" to="/recipe-generator" scrollY={scrollY} />
                     <NavItem icon={<FaUser />} text="Profile" to="/profile" scrollY={scrollY} />
                     <NavItem 
                       icon={<FaSignOutAlt />} 
@@ -138,6 +139,7 @@ const Navbar = () => {
                   <SidebarItem icon={<FaHome />} text="Home" to="/" onClick={toggleMenu} />
                   <SidebarItem icon={<FaUtensils />} text="Submit Recipe" to="/submit-recipe" onClick={toggleMenu} />
                   <SidebarItem icon={<FaUser />} text="Meal Plan" to="/meal-planner" onClick={toggleMenu} />
+                  <SidebarItem icon={<FaUser />} text="Recipe generator" to="/recipe-generator" onClick={toggleMenu} />
                   <SidebarItem icon={<FaUser />} text="Profile" to="/profile" onClick={toggleMenu} />
                   <SidebarItem 
                     icon={<FaSignOutAlt />} 
